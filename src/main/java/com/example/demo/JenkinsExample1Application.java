8cd7636575ef013a1315eee6aa838b7e92aebf80
harshag@00389 <harshadgaikwad14@gmail.com>
2020-10-07 13:09:52 +0530

    first version

---
+++ src/main/java/com/example/demo/JenkinsExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExample1Application.class, args);
+	}
+
+}
